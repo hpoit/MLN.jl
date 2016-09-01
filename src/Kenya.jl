9bcb23488bb98098f25719e1b4ce4cd45f2ae9d4
Henry Poitras <kvtliu@gmail.com>
2016-09-01 03:13:24 -0300

    Update Kenya.jl

--- src/Kenya.jl
+++ src/Kenya.jl
@@ -1,5 +1,22 @@
 module Kenya
 
-# package code goes here
+# This is a reference layout
 
-end # module
+# Database schema = 
+# Current directory = 
+# Temporary directory = 
+
+# Connect to RDBMS
+# Parse program file
+# Parse query fie
+# Parse operators
+# Decompose operators
+# Serialize execution plan
+
+# Parse evidence file
+# Store evidence
+# Optimize DMOs for {COREF} operator
+# Run {COREF} operator
+# Optimize DMOS for {LR} operator of label with relevant clauses partitioned by approximate sequence id
+
+end Kenya
