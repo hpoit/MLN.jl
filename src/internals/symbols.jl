c20172c2d3a30825fd474db1bd7c153e851f033b
Henry Poitras <kvtliu@gmail.com>
2016-09-02 16:04:28 -0300

    Create symbols.jl

---
+++ src/internals/symbols.jl
@@ -0,0 +1,2 @@
+# Uses a symbol table to convert all logic constants into integer IDs. Since MLN constants are usually typed, keeps track of 
+# constant type information. Note: a constant may be shared by multiple types.
