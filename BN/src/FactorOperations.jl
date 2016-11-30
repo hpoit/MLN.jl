7ffd12571ccfc1749044bd826c9c9d7c536fa2a7
Kevin Liu <kvtliu@gmail.com>
2016-11-30 16:05:59 -0200

    .

--- BN/src/FactorOperations.jl
+++ BN/src/FactorOperations.jl
@@ -18,14 +18,30 @@ function FactorProduct(A::Factor, B::Factor)
     A_common_idx = indexin(common_var, A.var)
     B_common_idx = indexin(common_var, B.var)
     if A.card[A_common_idx] != B.card[B_common_idx]
-        error("Inputs' common variables have mismatch cardinality!")
+        error("Variables in Factors A and B have cardinality mismatch!")
     end
 
     # Set the variables of C
     Cvar = union(A.var, B.var)
-
     Cvar_length = length(Cvar)
 
+    # Map [highest] index of variables of A and B in C
+    # mapA(i) = j iff A.var(i) == Cvar(j)
+    # mapB(i) = j iff B.var(i) == Cvar(j)
+
+    # E.g. If
+    # A.var = [3 1 4]
+    # B.var = [4 5]
+    # Cvar = [1 3 4 5] then
+
+    # mapA in Cvar = [2 1 3]
+    # mapB in Cvar = [3 4] for
+
+    # A.var(1) = 3
+    # Cvar(2) = 3 and
+
+    # A.var(1) == Cvar(2) index positions
+    # B.var(1) == Cvar(3) index positions
     mapA = indexin(A.var, Cvar)
     mapB = indexin(B.var, Cvar)
 
@@ -34,6 +50,7 @@ function FactorProduct(A::Factor, B::Factor)
     Ccard[mapA] = A.card
     Ccard[mapB] = B.card
 
+    # To calculate Cvaluespace, compute helper indices
     Aarr0 = indexin(Cvar, A.var)
     Barr0 = indexin(Cvar, B.var)
     Aarr1 = Aarr0[Aarr0.>0]
@@ -47,6 +64,7 @@ function FactorProduct(A::Factor, B::Factor)
     A1card[mapA] = A.card
     B1card[mapB] = B.card
 
+    # Populate factor values of C
     Avaluespace = reshape(A1.value, A1card...)
     Bvaluespace = reshape(B1.value, B1card...)
 
@@ -55,10 +73,10 @@ function FactorProduct(A::Factor, B::Factor)
     Factor(Cvar, Ccard, Cvaluespace[:])
 end
 
-
 *(A::Factor, B::Factor) = FactorProduct(A::Factor, B::Factor)
 
 
+# Factor marginalization: computes the factor without the variables of Remove_var
 function FactorMargin(A::Factor, Remove_var::Vector{String}, Remain_var::Vector{String}, Remove_dims::Vector{Int}, Remain_dims::Vector{Int})
     Remain_card = A.card[Remain_dims]
 
@@ -68,7 +86,8 @@ function FactorMargin(A::Factor, Remove_var::Vector{String}, Remain_var::Vector{
 
     squeeze_dims = [length(Remain_dims)+1:length(permute_dims)]
 
-    sumvaluespace = sum(permuted_valuespace, squeeze_dims)
+    # Sum elements of an array over the given dimensions.
+    sumvaluespace = sum(permuted_valuespace, Remain_dims)
     Remain_valuespace = squeeze(sumvaluespace, squeeze_dims)
 
     Factor(Remain_var, Remain_card, Remain_valuespace[:])
